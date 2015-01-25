25a2ec96eb21af6f75f488b457dbab604ebc2de0
Andres Vahter <andres.vahter@gmail.com>
2015-01-25 13:15:15 +0200

    read iq data from input and output it unmodified
    replace printf with fprintf and add timestamping

--- doppler.c
+++ doppler.c
@@ -35,6 +35,8 @@
 
 #define SPEED_OF_LIGHT_M_S 299792458.
 
+#define INPUT_STREAM_BLOCK_SIZE 8192
+
 typedef struct {
 	int arg_samplerate;
 	int samplerate;
@@ -63,22 +65,30 @@ typedef struct {
 	char output_file[OUTPUT_FILE_NAME_LEN];
 } args_t;
 
+char* utc_timestamp(){
+	static char timestamp[50];
+	time_t utime;
+	utime = time(NULL);
+	strftime(timestamp, 50, "%Y-%m-%dT%H:%M:%S.000", gmtime(&utime));
+	return timestamp;
+}
+
 void print_help() {
-	printf("doppler\t(C) 2015 Andres Vahter (andres.vahter@gmail.com)\n\n");
-	printf("doppler takes signed 16 bit IQ data stream as input and produces doppler corrected or constant shifted output\n");
-	printf("usage: doppler args\n");
-	printf("\t--const \t-c \t\t\t: constant shift mode: needs also --freq parameter\n");
-	printf("\t--doppler \t-d \t\t\t: doppler correction mode: needs also --freq, --tlefile, --tlename and --location parameters\n\n");
+	fprintf(stderr, "doppler\t(C) 2015 Andres Vahter (andres.vahter@gmail.com)\n\n");
+	fprintf(stderr, "doppler takes signed 16 bit IQ data stream as input and produces doppler corrected or constant shifted output\n");
+	fprintf(stderr, "usage: doppler args\n");
+	fprintf(stderr, "\t--const \t-c \t\t\t: constant shift mode: needs also --freq parameter\n");
+	fprintf(stderr, "\t--doppler \t-d \t\t\t: doppler correction mode: needs also --freq, --tlefile, --tlename and --location parameters\n\n");
 
-	printf("\t--samplerate \t-s <samplerate>\t\t: input data stream samplerate\n");
-	printf("\t--tlefile \t-t <filename>\t\t: doppler: TLE file\n");
-	printf("\t--tlename \t-n <name>\t\t: doppler: which TLE to use from TLE file\n");
-	printf("\t--location \t-l <lat,lon,alt>\t: doppler: specifies observer location on earth\n");
-	printf("\t--freq \t\t-f <freq_hz>\t\t: doppler: specifies object transmission frequency in Hz\n");
-	printf("\t\t\t\t\t\t: const: specifies by how much input stream will be shifted in Hz\n\n");
+	fprintf(stderr, "\t--samplerate \t-s <samplerate>\t\t: input data stream samplerate\n");
+	fprintf(stderr, "\t--tlefile \t-t <filename>\t\t: doppler: TLE file\n");
+	fprintf(stderr, "\t--tlename \t-n <name>\t\t: doppler: which TLE to use from TLE file\n");
+	fprintf(stderr, "\t--location \t-l <lat,lon,alt>\t: doppler: specifies observer location on earth\n");
+	fprintf(stderr, "\t--freq \t\t-f <freq_hz>\t\t: doppler: specifies object transmission frequency in Hz\n");
+	fprintf(stderr, "\t\t\t\t\t\t: const: specifies by how much input stream will be shifted in Hz\n\n");
 
-	printf("\t--output \t-o <filename>\t\t: logs information about frequnecy shifting to a file\n");
-	printf("\t--help \t\t-h \t\t\t: prints this usage information\n");
+	fprintf(stderr, "\t--output \t-o <filename>\t\t: logs information about frequnecy shifting to a file\n");
+	fprintf(stderr, "\t--help \t\t-h \t\t\t: prints this usage information\n");
 }
 
 int main(int argc, char *argv[]) {
@@ -125,7 +135,7 @@ int main(int argc, char *argv[]) {
 				args.arg_samplerate = 1;
 				args.samplerate = atoi(optarg);
 				if (args.samplerate < 1) {
-					printf("samplerate must be > 0\n");
+					fprintf(stderr, "samplerate must be > 0\n");
 					exit(EXIT_FAILURE);
 				}
 				break;
@@ -145,7 +155,7 @@ int main(int argc, char *argv[]) {
 					memcpy(&(args.tlefile[0]), optarg, strlen(optarg));
 				}
 				else {
-					printf("--tlefile (-t) argument %s is longer than %u, cannot use it as input!\n", optarg, TLE_FILE_NAME_LEN);
+					fprintf(stderr, "--tlefile (-t) argument %s is longer than %u, cannot use it as input!\n", optarg, TLE_FILE_NAME_LEN);
 					exit(EXIT_FAILURE);
 				}
 				break;
@@ -155,7 +165,7 @@ int main(int argc, char *argv[]) {
 					memcpy(&(args.tlename[0]), optarg, strlen(optarg));
 				}
 				else {
-					printf("--tlename (-n) argument %s is longer than %u, cannot use it as input!\n", optarg, TLE_NAME_FIELD_LEN);
+					fprintf(stderr, "--tlename (-n) argument %s is longer than %u, cannot use it as input!\n", optarg, TLE_NAME_FIELD_LEN);
 					exit(EXIT_FAILURE);
 				}
 				break;
@@ -163,7 +173,7 @@ int main(int argc, char *argv[]) {
 				subopts = optarg;
 				while (*subopts != '\0') {
 					char* saved = subopts;
-					switch(getsubopt(&subopts, (char **)location_opts, &value)) {
+					switch (getsubopt(&subopts, (char **)location_opts, &value)) {
 						case LAT_OPTION:
 							args.arg_lat = 1;
 							args.lat = strtod(value, NULL);
@@ -177,26 +187,26 @@ int main(int argc, char *argv[]) {
 							args.alt = strtod(value, NULL);
 							break;
 						default:
-							printf("incorrect suboption: '%s'\n", saved);
-							printf("correct usage is: --location (-l) lat=58.64560,lon=23.15163,alt=7.8\n");
+							fprintf(stderr, "incorrect suboption: '%s'\n", saved);
+							fprintf(stderr, "correct usage is: --location (-l) lat=58.64560,lon=23.15163,alt=7.8\n");
 							exit(EXIT_FAILURE);
 					}
 				}
 
 				if (!args.arg_lat) {
-					printf("'lat' is not specified with --location (-l) argument\n");
+					fprintf(stderr, "'lat' is not specified with --location (-l) argument\n");
 				}
 
 				if (!args.arg_lon) {
-					printf("'lon' is not specified with --location (-l) argument\n");
+					fprintf(stderr, "'lon' is not specified with --location (-l) argument\n");
 				}
 
 				if (!args.arg_alt) {
-					printf("'alt' is not specified with --location (-l) argument\n");
+					fprintf(stderr, "'alt' is not specified with --location (-l) argument\n");
 				}
 
 				if (!args.arg_lat || !args.arg_lon || !args.arg_alt) {
-					printf("correct usage is: --location (-l) lat=58.64560,lon=23.15163,alt=7.8\n");
+					fprintf(stderr, "correct usage is: --location (-l) lat=58.64560,lon=23.15163,alt=7.8\n");
 					exit(EXIT_FAILURE);
 				}
 				break;
@@ -206,7 +216,7 @@ int main(int argc, char *argv[]) {
 					memcpy(&(args.output_file[0]), optarg, strlen(optarg));
 				}
 				else {
-					printf("--output (-o) argument %s is longer than %u, cannot use it as input!\n", optarg, OUTPUT_FILE_NAME_LEN);
+					fprintf(stderr, "--output (-o) argument %s is longer than %u, cannot use it as input!\n", optarg, OUTPUT_FILE_NAME_LEN);
 					exit(EXIT_FAILURE);
 				}
 				break;
@@ -222,47 +232,47 @@ int main(int argc, char *argv[]) {
 
 	// arg samplerate
 	if (!args.arg_samplerate) {
-		printf("samplerate not specified!\n");
+		fprintf(stderr, "samplerate not specified!\n");
 		exit(EXIT_FAILURE);
 	}
 	else {
-		printf("samplerate: %u\n", args.samplerate);
+		fprintf(stderr, "samplerate: %u\n", args.samplerate);
 	}
 
 	// arg const mode
 	if (args.arg_const_mode && args.arg_doppler_mode) {
-		printf("--const (-c) and --doppler (-d) arguments cannot be used together\n");
+		fprintf(stderr, "--const (-c) and --doppler (-d) arguments cannot be used together\n");
 		exit(EXIT_FAILURE);
 	}
 
 	if (args.arg_const_mode && args.arg_freq_hz) {
-		printf("constant shift mode with %d Hz shift\n", args.freq_hz);
+		fprintf(stderr, "constant shift mode with %d Hz shift\n", args.freq_hz);
 	}
 	else if (args.arg_const_mode && !args.arg_freq_hz) {
-		printf("constant shift mode also needs --freq (-f) argument to know how much to shift\n");
+		fprintf(stderr, "constant shift mode also needs --freq (-f) argument to know how much to shift\n");
 		exit(EXIT_FAILURE);
 	}
 
 	// check which doppler mode parameter is missing
 	if (args.arg_doppler_mode && !args.arg_freq_hz) {
-		printf("doppler mode also needs --freq (-f) parameter which specifies object transmission frequency, ");
-		printf("for example 'ESTCUBE 1' uses 437505000 Hz\n");
+		fprintf(stderr, "doppler mode also needs --freq (-f) parameter which specifies object transmission frequency, ");
+		fprintf(stderr, "for example 'ESTCUBE 1' uses 437505000 Hz\n");
 	}
 	if (args.arg_doppler_mode && !args.arg_tlefile) {
-		printf("doppler mode also needs --tlefile (-t) parameter which specifies file with TLEs\n");
-		printf("such file can be downloaded from: https://celestrak.com/NORAD/elements/cubesat.txt\n");
+		fprintf(stderr, "doppler mode also needs --tlefile (-t) parameter which specifies file with TLEs\n");
+		fprintf(stderr, "such file can be downloaded from: https://celestrak.com/NORAD/elements/cubesat.txt\n");
 	}
 	if (args.arg_doppler_mode && !args.arg_tlename) {
-		printf("doppler mode also needs --tlename (-n) parameter which specifies which TLE to use from TLE file\n");
-		printf("for example use as --tlename -n 'ESTCUBE 1'\n");
+		fprintf(stderr, "doppler mode also needs --tlename (-n) parameter which specifies which TLE to use from TLE file\n");
+		fprintf(stderr, "for example use as --tlename -n 'ESTCUBE 1'\n");
 	}
 	if (args.arg_doppler_mode && (!args.arg_lat || !args.arg_lon || !args.arg_alt)) {
-		printf("doppler mode also needs --location (-l) parameter which specifies observer location\n");
-		printf("for example use as --location (-l) lat=58.64560,lon=23.15163,alt=7.8\n");
+		fprintf(stderr, "doppler mode also needs --location (-l) parameter which specifies observer location\n");
+		fprintf(stderr, "for example use as --location (-l) lat=58.64560,lon=23.15163,alt=7.8\n");
 	}
 
 	if (args.arg_doppler_mode && (!args.arg_freq_hz || !args.arg_tlefile || !args.arg_tlename || !args.arg_lat || !args.arg_lon || !args.arg_alt)) {
-		printf("\ndoppler mode example command:\n\tdoppler -s 1024000 -d -f 437505000 -t cubesats.txt -n 'ESTCUBE 1' --location lat=58.64560,lon=23.15163,alt=7.8 -o dopplet.out\n");
+		fprintf(stderr, "\ndoppler mode example command:\n\tdoppler -s 1024000 -d -f 437505000 -t cubesats.txt -n 'ESTCUBE 1' --location lat=58.64560,lon=23.15163,alt=7.8 -o dopplet.out\n");
 		exit(EXIT_FAILURE);
 	}
 
@@ -271,12 +281,16 @@ int main(int argc, char *argv[]) {
 		FILE* outputfp;
 		sat_t sat;
 		geodetic_t observer_location;
-		double doppler;
 
-		printf("doppler correction mode\n");
-		printf("\tTLE file: %s\n", args.tlefile);
-		printf("\tTLE name: %s\n", args.tlename);
-		printf("\tobserver location: lat %2.4f, lon %2.4f, alt %.1f m\n", args.lat, args.lon, args.alt);
+		double doppler;
+		uint8_t iq_buffer[INPUT_STREAM_BLOCK_SIZE];
+		int bytes_read;
+		clock_t last_print_time;
+
+		fprintf(stderr, "doppler correction mode\n");
+		fprintf(stderr, "\tTLE file: %s\n", args.tlefile);
+		fprintf(stderr, "\tTLE name: %s\n", args.tlename);
+		fprintf(stderr, "\tobserver location: lat %2.4f, lon %2.4f, alt %.1f m\n", args.lat, args.lon, args.alt);
 
 		observer_location.lat = Radians(args.lat);
 		observer_location.lon = Radians(args.lon);
@@ -287,29 +301,46 @@ int main(int argc, char *argv[]) {
 
 		// arg output file
 		if (args.arg_output_file) {
-			printf("write events to file: %s\n", args.output_file);
+			fprintf(stderr, "write events to file: %s\n", args.output_file);
 			outputfp = fopen(args.output_file, "w");
 			if (outputfp == NULL) {
-				printf("cannot open events output file %s\n", args.output_file);
+				fprintf(stderr, "cannot open events output file %s\n", args.output_file);
 				exit(EXIT_FAILURE);
 			}
 		}
+
+		last_print_time = clock();
 
 		while (1) {
 			predict_calc(&sat, &observer_location, predict_get_current_daynum());
 			doppler = (sat.range_rate * 1000 / SPEED_OF_LIGHT_M_S) * args.freq_hz * (-1.0);
 
-			if (args.arg_output_file) {
-				fprintf(outputfp, "[%s] jday: %12.5f, az:%6.1f, el:%6.1f, range rate:%6.3f km/s\n", args.tlename, sat.jul_utc, sat.az, sat.el, sat.range_rate);
-				fprintf(outputfp, "%3.3f MHz doppler: %6.1f Hz\n", args.freq_hz/1e+6, doppler);
-				fflush(outputfp);
-			}
-			else {
-				printf("\n[%s] jday: %12.5f, az:%6.1f, el:%6.1f, range rate:%6.3f km/s\n", args.tlename, sat.jul_utc, sat.az, sat.el, sat.range_rate);
-				printf("%3.3f MHz doppler: %6.1f Hz\n", args.freq_hz/1e+6, doppler);
+			if (((clock() - last_print_time) / CLOCKS_PER_SEC) > 0.01) {
+				last_print_time = clock();
+				if (args.arg_output_file) {
+					fprintf(outputfp, "%s: jday: %12.5f, az:%6.1f, el:%6.1f, range rate:%6.3f km/s\n", utc_timestamp(), sat.jul_utc, sat.az, sat.el, sat.range_rate);
+					fprintf(outputfp, "%s: %3.3f MHz doppler: %6.1f Hz\n", utc_timestamp(), args.freq_hz/1e+6, doppler);
+					fflush(outputfp);
+				}
+				else {
+					fprintf(stderr, "\n%s: jday: %12.5f, az:%6.1f, el:%6.1f, range rate:%6.3f km/s\n", utc_timestamp(), sat.jul_utc, sat.az, sat.el, sat.range_rate);
+					fprintf(stderr, "%s: %3.3f MHz doppler: %6.1f Hz\n", utc_timestamp(), args.freq_hz/1e+6, doppler);
+					fflush(stderr);
+				}
 			}
 
-			sleep(1);
+			// read IQ stream
+			// do doppler correction
+			// write IQ stream
+			bytes_read = fread(iq_buffer, 1, INPUT_STREAM_BLOCK_SIZE, stdin);
+			if (bytes_read) {
+				fwrite(iq_buffer, 1, bytes_read, stdout);
+				fflush(stdout);
+			}
+
+			if (feof(stdin)) {
+				break;
+			}
 		}
 
 		if (args.arg_output_file) {
